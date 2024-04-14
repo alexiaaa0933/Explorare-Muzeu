7f28b877686596501d1c917127819fba5dd7d73f
FitaRobert <fita.robert@yahoo.com>
2024-04-14 13:25:59 +0300

    Info Panel for paintings

---
+++ ExplorareMuzeu/Assets/Scripts/PaintingsText.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintingsText : MonoBehaviour
+{
+
+    public Renderer objectRenderer;
+
+    void Start()
+    {
+        // Verifică dacă obiectul renderer este valid
+        if (objectRenderer != null)
+        {
+            // Dezactivează rendererul pentru a face obiectul invizibil
+            objectRenderer.enabled = false;
+        }
+        else
+        {
+            // Dacă nu există renderer, afișează un mesaj de avertisment
+            Debug.LogWarning("Renderer component not assigned!");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // Verifică dacă obiectul care a intrat în collider este jucătorul
+        if (other.CompareTag("Player"))
+        {
+            // Activează rendererul pentru a face obiectul vizibil
+            objectRenderer.enabled = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        // Verifică dacă obiectul care a ieșit din collider este jucătorul
+        if (other.CompareTag("Player"))
+        {
+            // Dezactivează rendererul pentru a face obiectul invizibil
+            objectRenderer.enabled = false;
+        }
+    }
+}
